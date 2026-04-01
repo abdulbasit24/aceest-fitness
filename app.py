04f0fdfa3a039cae3a86d2b4e9bcec88f80099f1
abdulbasit <abdulbasitindia24@gmail.com>
2026-04-02 00:59:44 +0530

    fix: final flake8 issues

--- app.py
+++ app.py
@@ -131,3 +131,4 @@ def bmi_endpoint():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
+    
--- test_app.py
+++ test_app.py
@@ -205,3 +205,4 @@ class TestBMIEndpoint:
     def test_missing_weight(self, client):
         r = client.get("/bmi?height=175")
         assert r.status_code == 400
+        
